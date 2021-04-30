6af29239b4610345ab612f463da76dfc426e6b59
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-04-30 08:59:37 -0300

    Add generic

--- Impressao/PrintService.cs
+++ Impressao/PrintService.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace Impressao
 {
-    class PrintService
+    class PrintService<T>
     {
-        private int[] _values = new int[10];
+        private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(int value)
+        public void AddValue(T value)
         {
             if (_count == 10)
             {
@@ -19,7 +19,7 @@ namespace Impressao
             _count++;
         }
 
-        public int First()
+        public T First()
         {
             if (_count == 0)
             {
--- Impressao/Program.cs
+++ Impressao/Program.cs
@@ -6,7 +6,7 @@ namespace Impressao
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
